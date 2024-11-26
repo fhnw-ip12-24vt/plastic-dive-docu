fb8b423fa733d52cdaa891a8d7ba5eae976c2218
Klemen-Z <klemen.zmahar@students.fhnw.ch>
2024-11-26 10:21:06 +0100

    prototype structure corrected

---
+++ IP12-Prototype/src/main/ch/IP12/prototype/App.java
@@ -0,0 +1,7 @@
+package ch.IP12.prototype;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
--- IP12-Prototype/src/main/java/ch/IP12/prototype/App.java
+++
@@ -1,7 +0,0 @@
-package java.ch.IP12.prototype;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
