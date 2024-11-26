7f5656191ee789ae73dee79758b1a50b1706a0f8
Simtox <106579491+Simtox@users.noreply.github.com>
2024-11-26 10:56:38 +0100

    First commit for JavaFX

--- IP12-Prototype/src/main/ch/IP12/prototype/App.java
+++ IP12-Prototype/src/main/ch/IP12/prototype/App.java
@@ -1,7 +1,15 @@
 package ch.IP12.prototype;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) {
     System.out.println("Hello World!");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
