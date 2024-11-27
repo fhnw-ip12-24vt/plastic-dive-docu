5375f50e698f41227d263d3621e9b433b20c95d9
Klemen-Z <klemen.zmahar@students.fhnw.ch>
2024-11-27 11:54:55 +0100

    wacky obstacle movement

--- IP12-Prototype/src/main/ch/IP12/prototype/model/Obstacle.java
+++ IP12-Prototype/src/main/ch/IP12/prototype/model/Obstacle.java
@@ -1,9 +1,27 @@
 package main.ch.IP12.prototype.model;
 
 public class Obstacle extends Moveable{
+    private boolean waveUp = false;
     public Obstacle(int x, int y, int speed, int length, int height, String spritePath) {
         super(x, y, speed, length, height, spritePath);
         //forces the Obstacle to move to the left side of the screen.
         this.direction = 180;
+    }
+
+    @Override
+    public void update(double deltaTime){
+        if (waveUp){
+            direction--;
+            if (direction < 135){
+                waveUp = false;
+            }
+
+        } else {
+            direction++;
+            if (direction > 225){
+                waveUp = true;
+            }
+        }
+        move();
     }
 }
