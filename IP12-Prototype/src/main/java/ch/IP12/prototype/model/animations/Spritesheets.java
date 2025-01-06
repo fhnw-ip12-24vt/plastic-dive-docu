456112aa8aeee3f165fee8f172be771ea5144a1e
Klemen-Z <klemen.zmahar@students.fhnw.ch>
2025-01-06 09:10:49 +0100

    fish hitbox fix

--- IP12-Prototype/src/main/java/ch/IP12/prototype/model/animations/Spritesheets.java
+++ IP12-Prototype/src/main/java/ch/IP12/prototype/model/animations/Spritesheets.java
@@ -8,7 +8,7 @@ import java.util.Random;
 
 public enum Spritesheets {
     //to add more obstacle variations add an entry to the list bellow.
-    Player(new SpriteAnimation(new ImageView(new Image("player.png")), 2,2,0,0,16, 16, Duration.millis(200))),
+    Player(new SpriteAnimation(new ImageView(new Image("player.png")), 2,2,0,1,16, 7, Duration.millis(200))),
     SmallCluster(new SpriteAnimation(new ImageView(new Image("https://upload.wikimedia.org/wikipedia/commons/7/73/The_Horse_in_Motion.jpg")), 0,0,0,0,20, 20, Duration.millis(1000))),
     MediumCluster(new SpriteAnimation(new ImageView(new Image("https://upload.wikimedia.org/wikipedia/commons/7/73/The_Horse_in_Motion.jpg")), 0,0,0,0,35, 35, Duration.millis(1000))),
     LargeCluster(new SpriteAnimation(new ImageView(new Image("https://upload.wikimedia.org/wikipedia/commons/7/73/The_Horse_in_Motion.jpg")), 0,0,0,0,50, 50, Duration.millis(1000))),
