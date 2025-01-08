381a1ffe185a8e504d4c4a7395d70627cd5d96e7
Klemen-Z <klemen.zmahar@students.fhnw.ch>
2025-01-08 12:07:52 +0100

    attempts at fixing running on pi, it now outputs the error into IDE console

--- IP12-Prototype/src/assembly/startInDebugMode.sh
+++ IP12-Prototype/src/assembly/startInDebugMode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd "$1"
+cd /home/pi/deploy/IP12-Prototype
 pkill java
-DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java -XX:+UseZGC -Xmx1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar "$2".jar
+DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java -XX:+UseZGC -Xmx1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar IP12-Prototype-1.0-shaded.jar
 exit 0
