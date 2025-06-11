0c75cc20d67f68302a61be6a89dd8c25e29f7b25
Klemen-Z <klemen.zmahar@students.fhnw.ch>
2025-06-11 12:39:00 +0200

    removed prototype code from documentation repo

--- IP12-Prototype/src/main/java/ch/IP12/prototype/Main.java
+++
@@ -1,7 +0,0 @@
-package ch.IP12.prototype;
-
-public class Main {
-    public static void main(String[] args) {
-        App.main(args);
-    }
-}
--- PI4JTests/src/assembly/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/pi/deploy/HelloPi
-pkill java
-DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java -XX:+UseZGC -Xmx1G -jar HelloPi.jar
-exit 0
